db85ddb169b8c06f75382bab01007163386191d6
shaxzod9806 <sahzods252@gmail.com>
2021-09-07 21:11:11 +0500

    first commit

--- src/main/java/codefores/Numbers.java
+++ src/main/java/codefores/Numbers.java
@@ -5,7 +5,6 @@ public class Numbers {
 //        numbers
 
         System.out.println(oneRoomNumber(66));
-        System.out.println(oneRoomNumber(66));
     }
 
     public static boolean oneRoomNumber(int number){
